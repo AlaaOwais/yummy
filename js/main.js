099702f3c6cb4d8d41f0d57923b3c18822c7a250
Alaa <alaaewais208@gmail.com>
2025-06-02 21:03:44 +0300

    Update code

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 let sidebar = document.querySelector(".aside-btn");
 let aside = document.querySelector("aside");
-let closeIcon = document.querySelector(".bar-icon");
+let closeIcon = document.querySelector(".bar-main-icon");
 let elementsList = document.getElementsByClassName("element-list");
 sidebar.addEventListener("click", function () {
     aside.classList.toggle('collapsed-aside');
@@ -11,9 +11,12 @@ sidebar.addEventListener("click", function () {
         elementsList[i].classList.toggle("moving_down");
     }
 })
-
-
 let main = document.querySelector("main")
+main.addEventListener('click', function(){
+    aside.classList.remove('collapsed-aside');
+    closeIcon.classList.remove("fa-xmark");
+    closeIcon.classList.add("fa-bars");
+})
 
 async function homePage(){
     let request = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=`)
@@ -26,7 +29,7 @@ async function homePage(){
     {
         homeHTML+=`
                     <div class=" col-lg-3 text-white overflow-hidden  ">
-                        <div onclick="getMealngredients('${data.idMeal}')")" class="category-contant position-relative">
+                        <div onclick="getMealngredients('${data[i].idMeal}')")" class="category-contant position-relative">
                             <img class="image w-100" src="${data[i].strMealThumb}" alt="category photo">
                                 <div class="details d-flex  align-items-center">
                                     <h2 class="text-center">${data[i].strMeal}</h2>
@@ -49,6 +52,7 @@ let container = document.querySelector("#container")
 let contant = document.querySelector('div#contant')
 function showSearchInputs()
 {
+    main.innerHTML = ``
     
     container.innerHTML= `
     <div class="container">
@@ -343,31 +347,31 @@ function showContacts(){
         <div class="container w-75 text-center">
             <div class="row g-4">
                 <div class="col-md-6">
-                    <input id="nameInput" onkeyup="isValid(${regex.nameRegex}, this)" type="text" class="form-control" placeholder="Enter Your Name">
+                    <input id="nameInput" onkeyup="isValid(${regex.nameRegex}, this,'name')" type="text" class="form-control" placeholder="Enter Your Name">
                     <div id="nameAlert" class="alert alert-danger w-100 mt-2 d-none">
                         Special characters and numbers not allowed
                     </div>
                 </div>
                 <div class="col-md-6">
-                    <input id="emailInput" onkeyup="isValid(${regex.email}, this)" type="email" class="form-control " placeholder="Enter Your Email">
+                    <input id="emailInput" onkeyup="isValid(${regex.email}, this , 'email')" type="email" class="form-control " placeholder="Enter Your Email">
                     <div id="emailAlert" class="alert alert-danger w-100 mt-2 d-none">
                         Email not valid *user@example.com
                     </div>
                 </div>
                 <div class="col-md-6">
-                    <input id="phoneInput" onkeyup="isValid(${regex.phonenumber}, this)" type="text" class="form-control " placeholder="Enter Your Phone">
+                    <input id="phoneInput" onkeyup="isValid(${regex.phonenumber}, this, 'phone')" type="text" class="form-control " placeholder="Enter Your Phone">
                     <div id="phoneAlert" class="alert alert-danger w-100 mt-2 d-none">
                         Enter valid Phone Number
                     </div>
                 </div>
                 <div class="col-md-6">
-                    <input id="ageInput" onkeyup="isValid(${regex.age}, this)" type="number" class="form-control " placeholder="Enter Your Age">
+                    <input id="ageInput" onkeyup="isValid(${regex.age}, this , 'age')" type="number" class="form-control " placeholder="Enter Your Age">
                     <div id="ageAlert" class="alert alert-danger w-100 mt-2 d-none">
                         Enter valid age
                     </div>
                 </div>
                 <div class="col-md-6">
-                    <input id="passwordInput" onkeyup="isValid(${regex.password}, this)" type="password" class="form-control " placeholder="Enter Your Password">
+                    <input id="passwordInput" name="password" onkeyup="isValid(${regex.password}, this , 'password')" type="password" class="form-control " placeholder="Enter Your Password">
                     <div id="passwordAlert" class="alert alert-danger w-100 mt-2 d-none">
                         Enter valid password *Minimum eight characters, at least one letter and one number:*
                     </div>
@@ -385,7 +389,6 @@ function showContacts(){
         </div>
         `
 }
-var flag = false;
 const regex= 
 {
     nameRegex : /^[a-zA-Z\s]{3,50}$/,
@@ -394,38 +397,45 @@ const regex=
     email :/^[\w.-]+@[\w-]+\.[a-zA-Z]{2,}$/,
     password : /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
 }
+const validationStatus = {
+    name: false,
+    phone: false,
+    age: false,
+    email: false,
+    password: false,
+    repassword: false
+};
 
 repasswordInput = document.getElementById('repasswordInput')
 function rePasswordValid(r){
     passwordInput = document.getElementById('passwordInput')
-    if (r.value === passwordInput.value)
+    if (r.value === passwordInput.value && validationStatus.password)
     {
         r.nextElementSibling.classList.replace("d-block" , "d-none");
-        flag = true;
+        validationStatus.repassword = true;
     }
     else
     {
         r.nextElementSibling.classList.replace("d-none" , "d-block")
-        flag = false;
+        validationStatus.repassword = false;
     }
     checkSubmitButton();
 }
-function isValid(regex , input){
+function isValid(regex , input , filedname){
     if(regex.test(input.value))
     {
         input.nextElementSibling.classList.replace("d-block" , "d-none")
-        flag = true;
+        validationStatus[filedname] = true;
     }
     else
     {
         input.nextElementSibling.classList.replace("d-none" , "d-block")
-        flag = false;
+        validationStatus[filedname] = false;
     }
+    checkSubmitButton()
 }
 function checkSubmitButton(){
     submitBtn = document.getElementById("submitBtn")
-    if(flag)
-    {
-        submitBtn.disabled = false;
-    }
+        const allValid = Object.values(validationStatus).every(status => status === true);
+        submitBtn.disabled = !allValid;
 }
